3eef102facc640403de9f01dbc6431fe223dcc56
Lucas Sousa <god-lucas@hotmail.com>
2021-09-08 16:19:26 -0300

    Update Vector3.kt

--- samueltm/boredom/math/linearalgebra/Vector3.kt
+++ samueltm/boredom/math/linearalgebra/Vector3.kt
@@ -70,14 +70,12 @@ class Vector3(var x: Double, var y: Double, var z: Double, var createNewInstance
   /**
    * Takes a matrix of dimensions 3x3 and multiplies it by this vector.
    */
-  fun multiply(m: Array<DoubleArray>): Vector3 {
-    return set(
-      (m[0][0] * x) + (m[0][1] * y) + (m[0][2] * z),
-      (m[1][0] * x) + (m[1][1] * y) + (m[1][2] * z),
-      (m[2][0] * x) + (m[2][1] * y) + (m[2][2] * z),
-      createNewInstances
-    )
-  }
+  fun multiply(m: Array<DoubleArray>) = set(
+    (m[0][0] * x) + (m[0][1] * y) + (m[0][2] * z),
+    (m[1][0] * x) + (m[1][1] * y) + (m[1][2] * z),
+    (m[2][0] * x) + (m[2][1] * y) + (m[2][2] * z),
+    createNewInstances
+  )
 
   /**
    * Normalizes this vector and returns itself.
